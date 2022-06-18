c826cc5667d6114984785774a4a3be9d903d4998
PaulaWiltiner <paulawiltiner.pw@gmail.com>
2022-06-18 14:44:07 -0300

    bonus

--- index.js
+++ index.js
@@ -21,50 +21,75 @@ let tweets = [
   },
 ];
 
+function addAvatar(list) {
+  const newList = list.map((elem) => {
+    const avatar = users.filter((item) => item.username === elem.username)[0]
+      .avatar;
+    return { ...elem, avatar: avatar };
+  });
+  return newList;
+}
+
 server.post("/sign-up", (request, response) => {
   const verifUser = users.some(
     (item) => item.username === request.body.username
   );
-  if (verifUser) {
-    response.send("Já existe esse usuário.");
+  if (request.body.username === "" || request.body.avatar === "") {
+    response.status(400).send("Todos os campos são obrigatórios!");
   } else {
-    const newUser = {
-      username: request.body.username,
-      avatar: request.body.avatar,
-    };
+    if (verifUser) {
+      response.status(201).send("Já existe esse usuário.");
+    } else {
+      const newUser = {
+        username: request.body.username,
+        avatar: request.body.avatar,
+      };
 
-    users.push(newUser);
-    response.send(users);
+      users.push(newUser);
+      response.status(201).send("OK");
+    }
   }
 });
 
 server.post("/tweets", (request, response) => {
   const newTweet = {
-    username: request.body.username,
+    username: request.headers.username,
     tweet: request.body.tweet,
   };
-
-  tweets.push(newTweet);
-  response.send("OK");
+  if (request.body.username === "" || request.body.tweet === "") {
+    response.status(400).send("Todos os campos são obrigatórios!");
+  } else {
+    tweets.push(newTweet);
+    response.status(201).send("OK");
+  }
 });
 
 server.get("/tweets", (request, response) => {
+  const page = request.query.page;
   if (tweets.length > 10) {
-    const listTweet = tweets.slice(tweets.length - 10, tweets.length);
-    const tenTweets = listTweet.map((elem) => {
-      const avatar = users.filter((item) => item.username === elem.username)[0]
-        .avatar;
-      return { ...elem, avatar: avatar };
-    });
-    response.send(tenTweets);
+    if (page !== "" && Number(page) > 1) {
+      const listTweet = tweets.slice(
+        tweets.length - 10 * (page - 1) - 11 < 0
+          ? 0
+          : tweets.length - 10 * (page - 1) - 11,
+        tweets.length - 10 * (page - 1)
+      );
+      const tenTweets = addAvatar(listTweet);
+      response.send(tenTweets);
+    } else {
+      response.status(400).send("Informe uma página válida!");
+    }
   } else {
-    const finalTweets = tweets.map((elem) => {
-      const avatar = users.filter((item) => item.username === elem.username)[0]
-        .avatar;
-      return { ...elem, avatar: avatar };
-    });
+    const finalTweets = addAvatar(tweets);
     response.send(finalTweets);
   }
+});
+
+server.get("/tweets/:username", (request, response) => {
+  const userName = request.params.username;
+  const userNameMatch = tweets.filter((item) => item.username === userName);
+  const userTweets = addAvatar(userNameMatch);
+  response.send(userTweets);
 });
 
 server.listen(5000);
